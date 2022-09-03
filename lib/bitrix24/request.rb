aeea609cea5627d870529db5f490ffd6725cd24f
gildemberg-santos <gildemberg.santos@gmail.com>
2022-09-03 12:20:26 -0300

    refatorar classe request

--- lib/bitrix24/request.rb
+++ lib/bitrix24/request.rb
@@ -1,31 +1,46 @@
 # frozen_string_literal: true
 
 module Bitrix24
+  # Request class for Bitrix24
   class Request
-    attr_reader :json, :status_code
-    attr_writer :access_token
-
     def initialize(url)
-      raise Bitrix24::Error, "URL is required" if url.blank?
+      raise Bitrix24::InvalidURIError, "URL is required" if url.blank?
 
       @url = url
+      @response = nil
     end
 
     def get
-      uri = URI.parse(@url)
-      http = Net::HTTP.new(uri.host, uri.port)
+      @response = http
+      json
+      if status_code != 200
+        raise Bitrix24::InvalidURIError, json["error_description"] if json["error"] == "PORTAL_DELETED"
+
+        raise Bitrix24::Error, json["error_description"]
+      end
+    end
+
+    def json
+      JSON.parse(@response.body)
+    end
+
+    def status_code
+      @response.code.to_i
+    end
+
+    private
+
+    def request_http
+      request = Net::HTTP::Get.new(@url.request_uri)
+      request["Content-Type"] = "application/json"
+      request
+    end
+
+    def http
+      http = Net::HTTP.new(@url.host, @url.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(uri.request_uri)
-      request["Content-Type"] = "application/json"
-      responde = http.request(request)
-
-      @json = JSON.parse(responde.body)
-      @status_code = responde.code.to_i
-
-      raise Bitrix24::Error, @json["error_description"] if @status_code != 200
-    rescue Bitrix24::Error => e
-      raise e
+      http.request(request_http)
     end
   end
 end
