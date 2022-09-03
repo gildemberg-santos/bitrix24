b37e947f5cd82545b80d1812086575301447a44d
gildemberg-santos <gildemberg.santos@gmail.com>
2022-09-03 12:20:01 -0300

    criar classe de erro para url invalidas

--- lib/bitrix24/error.rb
+++ lib/bitrix24/error.rb
@@ -4,11 +4,14 @@ module Bitrix24
   class Error < StandardError
     def initialize(*args)
       super(*args)
-      if Bitrix24.debug?
-        Dir.mkdir("log") unless Dir.exist?("log")
-        log = Logger.new("log/bitrix.log", 0, 100 * 1024 * 1024)
-        log.debug(args)
-      end
+      Bitrix24.logger
+    end
+  end
+
+  class InvalidURIError < StandardError
+    def initialize(*args)
+      super(*args)
+      Bitrix24.logger
     end
   end
 end
