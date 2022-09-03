39bd2fb95f2387b393797c06bef4011db61c0140
gildemberg-santos <gildemberg.santos@gmail.com>
2022-09-03 12:00:46 -0300

    incluir validações

--- lib/bitrix24/base.rb
+++ lib/bitrix24/base.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 module Bitrix24
-  class Base
-    def blank?(value)
-      value.nil? || value.to_s.empty? || value.to_s.strip.empty?
-    end
+  def self.validate_url(url)
+    raise Bitrix24::InvalidURIError, "URL is required" if url.blank?
+    raise Bitrix24::InvalidURIError, "URL is invalid" unless Bitrix24.validate_url?(url)
+  end
+
+  def self.validate_lead(lead)
+    raise Bitrix24::Error, "Lead fields is required" if lead.blank?
+  end
+
+  def self.validate_lead_id(id)
+    raise Bitrix24::Error, "Lead id must be an integer" unless id.is_a?(Integer)
+    raise Bitrix24::Error, "Lead id is required" if id.blank?
+  end
+
+  def self.validate_url?(url)
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
   end
 end
