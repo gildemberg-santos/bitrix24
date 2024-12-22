389f4ef2c769307441a888b29685203acb21d85f
gildemeber-santos <gildemeber.santos@gmail.com>
2024-12-22 11:56:46 -0300

    refactor: :recycle: Substituir importação da classe RequestV2 por Request no módulo Bitrix24

--- lib/bitrix24.rb
+++ lib/bitrix24.rb
@@ -8,6 +8,7 @@ require "active_support/time"
 require "logger"
 require "openssl"
 require "httparty"
+require "micro/case"
 
 module Bitrix24; end
 
@@ -16,5 +17,5 @@ require "bitrix24/version"
 require "bitrix24/endpoint"
 require "bitrix24/debug"
 require "bitrix24/error"
-require "bitrix24/request_v2"
+require "bitrix24/request"
 require "bitrix24/create_lead"
