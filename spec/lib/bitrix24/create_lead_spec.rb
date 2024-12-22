5d40b48920775c92061179e6611aeef17108b2a9
gildemberg-santos <gildemberg.santos@gmail.com>
2024-12-22 13:50:48 -0300

    test: :white_check_mark: Atualizar testes de criação de lead para usar subject e melhorar a verificação de falhas

--- spec/lib/bitrix24/create_lead_spec.rb
+++ spec/lib/bitrix24/create_lead_spec.rb
@@ -3,29 +3,31 @@
 require "spec_helper"
 
 RSpec.describe Bitrix24::CreateLead do
-  subject(:service) { described_class.call(url: url, lead_fields: lead_fields, custom_fields: custom_fields) }
+  subject { described_class.call(url: url, lead_fields: lead_fields, custom_fields: custom_fields) }
 
   let(:url) { "https://b24-iq2a30.bitrix24.com.br/rest/1/onkpa24vwd18zi90/" }
   let(:lead_fields) { { "TITLE" => "Test" } }
   let(:custom_fields) { [{ "name" => "TEST_DD", "value" => "VALOR_ADD" }] }
-  let(:fields) { { "FIELDS[TITLE]": "Test", "FIELDS[TEST_DD]": "VALOR_ADD" } }
-  let(:code) { 200 }
-  let(:body) { fields.to_json }
 
   describe "#call" do
     context "when success" do
       it "returns the expected response" do
         VCR.use_cassette("create_lead_success", record: :new_episodes) do
-          expect(service).to be_success
+          expect(subject).to be_success
+          expect(subject.data[:json][:result]).to eq(23)
         end
       end
     end
 
     context "when failure" do
-      let(:code) { 404 }
-      let(:body) { { error: nil, error_description: "Portal deleted" }.to_json }
+      let(:url) { "https://b24-iq2a30.bitrix24.com.br/rest/1/invalid/" }
 
-      it { expect { service }.to raise_error(Bitrix24::Error, "Portal deleted") }
+      it "raises an error" do
+        VCR.use_cassette("create_lead_failure", record: :new_episodes) do
+          expect(subject).to be_failure
+          expect(subject.data[:error]).to eq("Invalid request credentials")
+        end
+      end
     end
   end
 end
