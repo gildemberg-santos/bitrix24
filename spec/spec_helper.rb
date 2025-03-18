a8565ac7c26e6b7501fdb66148fb4bde7a81bf17
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-18 12:25:01 -0300

    refactor: :art: Simplificar a configuração do RSpec e remover dependências desnecessárias no spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,55 +2,20 @@
 
 require "rspec"
 require "vcr"
-require "pry"
-require "pry-reload"
-require "yaml"
-require "uri"
-require "json"
-require "net/http"
-require "active_support/time"
-require "logger"
+require "webmock/rspec"
 require "bitrix24"
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr_cassettes"
-  config.configure_rspec_metadata!
   config.hook_into :webmock
-  config.allow_http_connections_when_no_cassette = false
+  config.configure_rspec_metadata!
 end
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.define_derived_metadata do |metadata|
-    metadata[:aggregate_failures] = true
-  end
-
-  config.default_path = "spec/lib/bitrix24"
-
-  config.order = :random
-
-  config.full_backtrace = false
-
-  config.profile_examples = 10
-
-  config.filter_run_when_matching :focus
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  config.example_status_persistence_file_path = "spec/examples.txt"
-
+  config.example_status_persistence_file_path = ".rspec_status"
   config.disable_monkey_patching!
 
-  config.warnings = true
-
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
   end
 end
