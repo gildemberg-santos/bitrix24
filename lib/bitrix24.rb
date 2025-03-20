efff60c49c2e0a692fa866a81f8af6f4b270be22
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-19 22:38:23 -0300

    refactor: :art: Remover dependências não utilizadas no módulo Bitrix24

--- lib/bitrix24.rb
+++ lib/bitrix24.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
-require "yaml"
-require "uri"
-require "json"
-require "net/http"
 require "active_support/all"
-require "logger"
-require "openssl"
 require "httparty"
 require "u-case"
 
