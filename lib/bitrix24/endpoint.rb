123a3cef387fa2bac8f46f1e2c40100afe488ade
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-20 01:08:10 -0300

    refactor: :art: Atualizar comentários para descrever endpoints do CRM Lead

--- lib/bitrix24/endpoint.rb
+++ lib/bitrix24/endpoint.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Bitrix24
-  # Default fields endpoint
+  # INFO: CRM Lead
   ENDPOINT_ADD = "crm.lead.add"
   ENDPOINT_DELETE = "crm.lead.delete"
   ENDPOINT_GET = "crm.lead.get"
@@ -9,7 +9,7 @@ module Bitrix24
   ENDPOINT_UPDATE = "crm.lead.update"
   ENDPOINT_FIELDS = "crm.lead.fields"
 
-  # After custom fields
+  # INFO: CRM Lead User Field
   ENDPOINT_ADD_CUSTOM = "crm.lead.userfield.add"
   ENDPOINT_GET_CUSTOM = "crm.lead.userfield.get"
 end
