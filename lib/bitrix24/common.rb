2e5205a351700ac284b119504b812512495f068c
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-20 01:52:48 -0300

    refactor: :art: Adicionar classe Common para manter compatibilidade com o código original

---
+++ lib/bitrix24/common.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Bitrix24
+  # TODO: Just to maintain compatibility with the original code
+  class Common
+    attr_accessor :url, :lead_fields, :custom_fields
+
+    def initialize
+      @url = nil
+      @lead_fields = nil
+      @custom_fields = nil
+    end
+
+    def merge_fields_and_custom_fields(lead_fields, custom_fields)
+      @lead_fields = lead_fields
+      @custom_fields = custom_fields
+    end
+
+    def add(_)
+      response = Bitrix24::CreateLead.call(url: url, lead_fields: lead_fields, custom_fields: custom_fields)
+      raise Bitrix24::Error, response.data[:error] if response.failure?
+
+      response
+    end
+  end
+end
